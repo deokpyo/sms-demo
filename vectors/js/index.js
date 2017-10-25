e198a5152cc76c2ccca781ee2b16774dfda39f96
Jimmy Hong <jimmyhong.rocks@gmail.com>
2017-10-25 11:36:55 -0500

    theme integration

--- vectors/js/index.js
+++ vectors/js/index.js
@@ -1,33 +1,52 @@
-/* Define your vectors here. To run the samples, uncomment the functions
-   below then from the root directory, run the devserver:
-   $ turbo devserver
+// import npm packages here
+var superagent = require('superagent')
+var twilio = require('twilio')
 
-   then navigate to: 
-   http://localhost:3000/vectors/sample
-
-   Deploy the vectors by running from root directory:
-   $ turbo vectors
-*/
-
-// import npm packages here:
-// var turbo = require('turbo360')
-
+// define your functions here:
 module.exports = {
+	sms: (req, res) => {
+		if (req.query.message == null){
+			res.json({
+				confirmation: 'fail',
+				message: 'message parameter required.'
+			})
+			return
+		}
 
-	// sample: (req, res) => {
-	// 	res.status(200).json({
-	// 		confirmation: 'success',
-	// 		data: 'This is sample one!'
-	// 	})
-	// },
+		if (req.query.to == null){
+			res.json({
+				confirmation: 'fail',
+				message: 'to parameter required.'
+			})
+			return
+		}
 
-	// sampleTwo: (req, res) => {
-	// 	var queryParams = req.query
-	// 	res.status(200).json({
-	// 		confirmation: 'success',
-	// 		data: 'This is sample two!',
-	// 		params: queryParams
-	// 	})
-	// }
+		var accountSid = 'AC9c49e2d49c7bd26497abc336ca973acf' // Your Account SID from www.twilio.com/console
+		var authToken = 'aade7e7b97256b6a63183907c461f3bd' // Your Auth Token from www.twilio.com/console
 
+		var twilio = require('twilio')
+		var client = new twilio(accountSid, authToken)
+
+		let payload = null
+		client.messages.create({
+		    body: req.query.message,
+		    to: '+1'+req.query.to,  // Text this number
+		    from: '+16177444862' // From a valid Twilio number
+		})
+		.then((message) => {
+			payload = {
+				confirmation: 'success',
+				data: message.sid
+			}
+
+			res.json(payload)
+			return
+		})
+		.catch(err => {
+			res.json({
+				confirmation: 'fail',
+				message: err.message
+			})
+		})
+	}
 }
