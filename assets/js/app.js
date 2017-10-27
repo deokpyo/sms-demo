c3de50169ae00505adf950f3cb11750b1a9cce95
Jimmy Hong <jimmyhong.rocks@gmail.com>
2017-10-27 14:27:05 -0500

    update vector function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
 function onSendClick() {
     var phone = document.getElementById("phone").value;
     var msg = document.getElementById("message").value;
+    var apiKey = process.env.apiKey;
     if (phone && message) {
         console.log("SMS Requested");
         var param = {
+            key: apiKey,
             message: msg,
             to: phone
         }
-        $.get("/vectors/sms", param, function (res) {
+        $.get("https://production.turbo360-vector.com/sms-demo-6xh5gi/sms", param, function (res) {
             if (res.confirmation === "success") {
                 alert("Message has been sent!");
             } else {
